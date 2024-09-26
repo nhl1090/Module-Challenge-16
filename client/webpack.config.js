0d4adcd1d745cc744825f9b8b846e70e1ffd8dee
nhl1090 <neilhdz24@gmail.com>
2024-09-26 19:28:04 -0400

    Installed CSS loaders. Added rules to use the loaders and built the project along with dist folder and bundle.js files.

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -23,7 +23,10 @@ module.exports = () => {
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
       ],
     },
   };
